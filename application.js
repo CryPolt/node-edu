e9f240a252c6041a5eae8c1298f85d627ceadc00
crypolt <crypolt@gmail.com>
2024-05-23 19:12:54 +0500

    promise learning edu

--- application.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const port = process.env.PORT || 3500;
-
-http.createServer(function(req,res){
-    console.log('server work');
-    res.end('<!DOCTYPE html>');
-}).listen(port);
