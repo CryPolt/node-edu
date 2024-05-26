23960bf3178a2d9eac9fe8826ddcf7fe10539ad7
crypolt <crypolt@gmail.com>
2024-05-26 22:57:53 +0500

    попытки писать на vue + js / немного отложил чтобы разобраться полностью с проммисами и методами js

---
+++ vue-project/src/views/AboutView.vue
@@ -0,0 +1,35 @@
+<script setup>
+  import TheWelcome from "@/components/home/TheWelcome.vue";
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <br>
+    <div class="left">
+      <main>
+        <TheWelcome />
+      </main>
+    </div>
+
+  </div>
+
+</template>
+
+<style>
+.left {
+  float: right;
+  display: block;;
+  position: relative;
+  margin-left: 85px;
+}
+
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: inline-flex;
+    align-items: center;
+
+  }
+}
+</style>
---
+++ vue-project/src/views/ProjectView.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+
+  <div class="block">
+      <h1>projects</h1>
+  </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
