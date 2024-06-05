847386af64cb083539c98f60efa12132c4712632
crypolt <crypolt@gmail.com>
2024-06-05 23:29:35 +0500

    first edu vue

---
+++ vue-project/src/App.vue
@@ -0,0 +1,33 @@
+<script>
+
+export default {
+  data(){
+    return {
+      info: 'Title',
+      someinfo: 'someinfo'
+    }
+  },
+  methods: {
+    userData(word = 'New text') {
+      this.someinfo = word
+    }
+  }
+}
+
+</script>
+
+<template>
+    <h3>{{ info }}</h3>
+    <p>{{ someinfo }}</p>
+
+  <button type="button" v-on:click="userData('Some new')">send</button>
+</template>
+
+<style scoped>
+h3 {
+  font-weight: bold;
+}
+p {
+  color: red;
+}
+</style>
