e02140ea69253f0e4c571b02b5092967f6241fd7
crypolt <crypolt@gmail.com>
2024-06-05 23:57:36 +0500

    form senders edu start

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,13 +3,31 @@
 export default {
   data(){
     return {
-      info: 'Title',
-      someinfo: 'someinfo'
+      users: [],
+      error: '',
+      username: '',
+      userPassword: '',
+      userEmail: ''
     }
   },
   methods: {
-    userData(word = 'New text') {
-      this.someinfo = word
+    sendData() {
+      if (this.username === ''){
+        this.error = 'name not found';
+        return;
+      } else if(this.userEmail) {
+        this.error = 'email not found';
+        return;
+      } else if(this.userPassword) {
+        this.error = 'password not found';
+        return;
+      }
+
+      this.users.push({
+        name: this.username,
+        password: this.userPassword,
+        email: this.userEmail
+      })
     }
   }
 }
@@ -17,17 +35,26 @@ export default {
 </script>
 
 <template>
-    <h3>{{ info }}</h3>
-    <p>{{ someinfo }}</p>
 
-  <button type="button" v-on:click="userData('Some new')">send</button>
+  <form action="">
+    <input type="text" name="" id="" placeholder="name" v-model="username">
+    <input type="password" name="" id="" placeholder="password" v-model="userPassword">
+    <input type="email" name="" id="" placeholder="email" v-model="userEmail">
+    <p className="error"> {{error}}</p>
+    <button type="button" v-on:click="sendData()">send</button>
+
+    <div v-if="users.length === 0" className="users">
+      not users
+    </div>
+
+    <div v-for="(el,index) in users" :key="index">
+        <h3>{{el.name}}</h3>
+        <p>{{el.email}} - <b>{{el.password}}</b></p>
+    </div>
+  </form>
+
 </template>
 
 <style scoped>
-h3 {
-  font-weight: bold;
-}
-p {
-  color: red;
-}
+
 </style>
