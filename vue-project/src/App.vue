3cd8b44eb21f701412bdd5112f0ad272551e98af
crypolt <crypolt@gmail.com>
2024-06-07 00:37:20 +0500

    start writing api for giving weather in city

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,64 +1,101 @@
 <script>
-import User from "@/components/User.vue";
 
 export default {
-  components: { User },
-
   data(){
     return {
-      users: [],
-      error: '',
-      username: '',
-      userPassword: '',
-      userEmail: ''
+      city: "",
+      error: ""
     }
   },
   methods: {
 
-    sendDate() {
-
-    },
-    deleteUser(index){
-      this.users.splice(index, 1);
-      console.log(index, "Deleted")
-    },
-
-    sendData() {
-      if (this.username === ''){
-        this.error = 'name not found';
-      } else if(this.userPassword) {
-        this.error = 'password not found';
-      }else if(this.userEmail) {
-        this.error = 'email not found';
-        return;
+    getWeather(){
+      if (this.city.trim().length < 2) {
+        this.error = "more symbols | your < 2"
+        return false
       }
+      this.error = '';
+    }
 
-      this.users.push({
-        name: this.username,
-        password: this.userPassword,
-        email: this.userEmail
-      })
+
+  },
+  computed: {
+    cityName() {
+      return "<" + this.city + ">"
     }
-    }
+  }
 }
+ace24327d3a04f90c0e580874750c749
 
 </script>
 
 <template>
 
-    <input type="text" placeholder="name" v-model="username">
-    <input type="text" placeholder="password" v-model="userPassword">
-    <input type="email" placeholder="email" v-model="userEmail">
-    <p className="error"> {{error}}</p>
-    <button type="button" v-on:click="sendData()">send</button>
-    <div v-if="users.length === 0" className="users">
-      not users
-    </div>
-
-    <user v-for="(el,index) in users" :key="index" :user="el" :index="index" :deleteUser="deleteUser"/>
+  <div class="wrapper">
+    <h1>Weather app</h1>
+    <p>Weather in {{ city === '' ? "your city" : cityName}}</p>
+    <input type="text" placeholder="City" v-model="city">
+    <button v-if="city !== ''" @click="getWeather()">Weather</button>
+    <button disabled v-else>Weather input</button>
+    <p class="error">{{error}}</p>
+  </div>
 
 </template>
 
 <style scoped>
+
+.error {
+  color: red;
+}
+
+.wrapper{
+  width: 900px;
+  height: 500px;
+  border-radius: 50px;
+  background: darkblue;
+  padding: 20px;
+  text-align: center;
+  color: white;
+}
+.wrapper h1 {
+  margin-top: 50px;
+}
+.wrapper p {
+  margin-top: 20px;
+}
+.wrapper input {
+  margin-top: 30px;
+  background: transparent;
+  border: 0;
+  border-bottom: 2px solid black;
+  color: white;
+  font-size: 14px;
+  padding: 5px 8px;
+  outline: none;
+}
+.wrapper input:focus {
+  border-bottom-color: aquamarine;
+}
+
+
+.wrapper button:disabled {
+  border-bottom-color: red;
+  cursor: not-allowed;
+  background: #282828;
+}
+
+.wrapper button {
+  background: yellow;
+  color: olivedrab;
+  border-radius: 10px;
+  border: 2px solid chartreuse;
+  margin-left: 20px;
+  padding: 10px 15px;
+  cursor: pointer;
+  transition: transform 500ms ease;
+}
+.wrapper button:hover {
+  transform: Scale(1.1) translateY(-5px);
+}
 
 </style>
