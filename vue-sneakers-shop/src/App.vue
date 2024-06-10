9aa53fab54798d52c93913959feaf78dd6678b0b
crypolt <crypolt@gmail.com>
2024-06-10 23:59:24 +0500

    vue
    Начал делать верстку по макету с помощью tailwind
    
     https://www.figma.com/design/fw0toTyXMwM1y4WIe0YFrJ/React-Sneakers?node-id=0-1&t=MXxaxaHnwfEkv77Q-1

--- vue-sneakers-shop/src/App.vue
+++ vue-sneakers-shop/src/App.vue
@@ -1,17 +1,27 @@
 <script setup>
-import MyHello from "@/components/MyHello.vue";
+import Header from "@/components/header.vue";
 </script>
 
 <template>
-  <h1>Crypolt blog</h1>
-  <br>
-  <my-hello title="Puma" :price="9500" is-added>
-    <button>Add</button>
-  </my-hello>
-  <br>
-  <my-hello>
-    <h1>send</h1>
-  </my-hello>
-  <br>
-  <my-hello/>
+  <div class="w-4/5 m-auto bg-white h-screen rounded-xl shadow-xl mt-14">
+    <Header />
+
+    <div class="relative flex flex-col w-full border border-slate-100 rounded-xl p-8 cursor-pointer transition hover:shadow-xl hover:transform hover:-translate-y-2">
+      <div @click="onClick" class="absolute top-8 left-8">
+        <img :src="isLiked ? '/like-1.svg' : '/like-2.svg'" alt="Favorite" />
+      </div>
+      <img src="/sneakers/sneakers-1.jpg" class="w-full" alt="Sneaker" />
+      <p>{{ title }}</p>
+      <div class="flex justify-between mt-5">
+        <div class="flex flex-col gap-2">
+          <span class="text-slate-200">Цена:</span>
+          <span class="font-bold">{{ price }} руб.</span>
+        </div>
+        <img src="/plus.svg" alt="Plus" />
+      </div>
+    </div>
+
+  </div>
+
+
 </template>
---
+++ vue-sneakers-shop/src/components/header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+
+    <!--      leftside-->
+    <div class="flex items-center gap-4">
+      <img src="/logo.png" alt="Logo"  class="w-10"/>
+      <div>
+        <h2 class="text-xl font-bold uppercase">Sneakers</h2>
+        <p class="text-slate-500">Shop the best sneakers</p>
+      </div>
+    </div>
+
+    <!--      rightside-->
+    <ul class="flex items-center gap-10">
+      <li class="flex cursor-pointer items-center gap-3 text-gray-500 hover:text-black">
+        <img src="/cart.svg" alt="Cart">
+        <b>5000 usd</b>
+      </li>
+
+      <li class="flex cursor-pointer gap-3 text-gray-500 hover:text-black">
+        <img src="/heart.svg" alt="Cart">
+        <span>Favorite</span>
+      </li>
+
+      <li class="flex cursor-pointer gap-3 text-gray-500 hover:text-black">
+        <img src="/profile.svg" alt="Cart">
+        <span>Profile</span>
+      </li>
+    </ul>
+
+  </header>
+</template>
