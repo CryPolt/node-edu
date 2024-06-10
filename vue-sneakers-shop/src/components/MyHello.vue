29ea9314541dbcb4aabb0525617e3fa7ecc7ba64
Владос <poltavskiyvladislav@vk.com>
2024-06-10 21:36:56 +0500

    vue-sneakers-shop basic learning vue

---
+++ vue-sneakers-shop/src/components/MyHello.vue
@@ -0,0 +1,38 @@
+<script setup>
+// import {ref} from 'vue'
+//   const count = ref(0);
+//
+//   function increment() {
+//     count.value++
+//     console.log(count)
+//   }
+
+defineProps({
+  title: String,
+  price: Number,
+  isAdded: Boolean
+});
+
+</script>
+
+<template>
+<!--  <h3 class="text">{{ count }}</h3>-->
+<!--  <button @click="increment">Click</button>-->
+<!--  <input @keyup.ctrl.enter="increment">-->
+
+  <div class="product">
+    <h2>Product: {{title}}</h2>
+    <h3>Price: {{price}}</h3>
+    <h3>Added: {{isAdded}}</h3>
+  </div>
+
+</template>
+
+<style scoped>
+.product{
+  border: 1px solid black;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+
+</style>
